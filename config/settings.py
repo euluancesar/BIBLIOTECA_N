bee4f03e98b8e2daf5e463bcb75cc6a3ea6bb969
euluancesar <luancesarsilva15@gmail.com>
2025-06-02 07:41:08 -0300

    Add files via upload

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'crispy_forms',
     'crispy_bootstrap5', 
     'django.contrib.messages',
     'django.contrib.staticfiles',
